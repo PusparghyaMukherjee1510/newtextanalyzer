7e4c600b548f09a6facddc87fe9dc2b810c71c2a
PusparghyaMukherjee1510 <pusparghyamukherjee10@gmail.com>
2023-12-19 13:59:55 +0530

    git commit -amend

--- src/module1.mjs
+++
@@ -1,9 +0,0 @@
-const a="qwe"
-const b="rty"
-const c="uio"
-const d="opl"
-
-export default b;
-export {a}
-export {c}
-export {d}
--- src/module2.mjs
+++
@@ -1,6 +0,0 @@
-import ds ,{ a, d,c } from './module1.mjs';
-
-console.log(ds);
-console.log(c);
-console.log(d);
-console.log(a);
